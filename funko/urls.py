e5f34e92e188e9517991885adf5dec2a60aa6871
ocimar84 <ocimarcostaf@gmail.com>
2023-07-10 20:30:57 +0000

    django upgrade

--- funko/urls.py
+++ funko/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts', include('allath.url')),
+    path('accounts', include('allauth.url')),
 
 ]
